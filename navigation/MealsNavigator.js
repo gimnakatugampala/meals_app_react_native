983e7e1be819291ae58871645fa6acbf3ecb96d3
gimnakatugampala <gimnakatugampala1@gmail.com>
2021-10-28 12:17:34 +0530

    adding config & styles to bottom navigation tab

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Platform} from 'react-native'
 import { createStackNavigator , createBottomTabNavigator,createAppContainer } from "react-navigation";
 import { Ionicons } from '@expo/vector-icons';
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
 
 import CategoriesScreen from "../screens/CategoriesScreen";
 import CategoryMealsScreen from '../screens/CategoryMealsScreen'
@@ -33,22 +34,31 @@ const MealsNavigator = createStackNavigator({
     }
 })
 
-
-
-const MealsFavTabNavigator = createBottomTabNavigator({
+const tabConfig = {
     Meals:{screen:MealsNavigator,navigationOptions:{
         tabBarIcon:(tabInfo) => {
             return <Ionicons name='ios-restaurant' size={25} color={tabInfo.tintColor} />
-        }
+        },
+        tabBarColor:Colors.primaryColor
     }},
     Favorites:{screen:Favorites,
     navigationOptions:{
         tabBarIcon:(tabInfo) => {
             return <Ionicons name='ios-star' size={25} color={tabInfo.tintColor} />
-        }
+        },
+        tabBarColor:Colors.accentColor
+
     }
     }
-},{
+}
+
+
+
+const MealsFavTabNavigator = Platform.OS === 'android' ? createMaterialBottomTabNavigator(tabConfig,{
+    activeTintColor:'white',
+    shifting:true
+}) : 
+createBottomTabNavigator(tabConfig,{
     tabBarOptions:{
         activeTintColor:Colors.accentColor
     }
